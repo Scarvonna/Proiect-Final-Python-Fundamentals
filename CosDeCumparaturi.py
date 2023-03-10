cfd2afc3997ff7bcd89f4a0b4a801465922a348f
Belu Florentina-Alexandra <102697893+beluflorentina@users.noreply.github.com>
2023-03-10 03:11:11 +0200

    Update CosDeCumparaturi.py

--- CosDeCumparaturi.py
+++ CosDeCumparaturi.py
@@ -1,9 +1,13 @@
-class CosDeCumparaturi:
-    def __init__(self, lista_de_produse):
+class CosDeCumparaturi(Client):
+    def __init__(self, lista_de_produse):#lista ce contine liste [[]] =[[produs, cantitatea]] care poate fi CUMPARATE:
+                                         #exista in magazin si clientul are buget pentru ele
         self.lista_de_produse = lista_de_produse
 
-    def adauga_produs(self, produs, cantitate, pret):
-        print(produs, cantitate, pret)
+    def adauga_produs(self, produs, cantitate):
+        if cantitate>0:
+            return f"{produs}.....x{cantitate}"
+        else:
+            return "\n"
 
-    def elibereaza_bon(self):#nume produs, cantitate, pret, pret final
-            pass
+    def elibereaza_bon(self):
+        return "               Bon fiscal\n"
