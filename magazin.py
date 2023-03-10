f555c45c9b821cfebbf44502e61b642b087fbdb8
Belu Florentina-Alexandra <102697893+beluflorentina@users.noreply.github.com>
2023-03-10 03:15:23 +0200

    Update magazin.py

--- magazin.py
+++ magazin.py
@@ -4,92 +4,93 @@ import Produs
 import Client
 import CosDeCumparaturi
 
-f = open(sys.argv[1], mode='r')
-a = f.readlines()
-f.close()
-nr = 0
-produse = Produs.Produs([], [], [])
-for i in a:
-    b, c, d = a[nr].partition(",")
-    e, f, g = d.partition(",")  # complica m as mai rau
-    produse.nume.append(b), produse.stoc.append(int(e)), produse.pret.append(int(g.strip("\n")))#modificat prin int
-    nr += 1
-print(produse.nume, produse.stoc, produse.pret)
 
+#crearea stocului magazinului
+produse = Produs([], [], [])
+with open(sys.argv[1], mode='r') as f:  #context manager
+    for x in f.readlines():
+        a, b, c = x.partition(",")  #partition imparte stringul intr-o tupla cu 3 elemente ex:
+        d, e, f = c.partition(",")  #a=paine, b=",",c=50,3
+        produse.nume.append(a), produse.stoc.append(int(d)), produse.pret.append(int(f.strip("\n")))
+        # print(produse.nume, produse.stoc, produse.pret)
+
+#accesarea fisierelor text din folder
 os.chdir(sys.argv[2])
-lista=os.listdir(os.chdir(sys.argv[2]))
+lista = os.listdir(os.chdir(sys.argv[2]))  #lista cu "nume.txt" a fisierelor din folder
 
-numarul = 0
+a = str()
+key = []
+value = []
+cos=CosDeCumparaturi([])
 
+#listele de cumparaturi si bugetul clientilor
+for i in lista:
+    client = Client(int(), {})#i=fisier cu info despre fiecare client in parte
+                              #atributele obiectului client trebuie initiate cu "0" pentru fiecare client in parte
+    with open(i, mode='r') as o:
+        k = o.readlines()
 
-for i in lista:  # se face pentru fiecare client in parte asta
-    j = open(lista[numarul], mode="r")
-    k = j.readlines()# k este lista de cumparaturi a fiecarui client
-    j.close()
+        client.buget = int(k[0])#primul element din lista
+        #print(client.buget)
 
-    #creare instante Client
-    valoare = Client.Client([], [])
-    valoare.lista_de_produse={}
+        for a in k[1:]:#celelalte elemente din lista
+            key, b, value = a.partition(",")
+            client.lista_de_produse.update({key: int(value.strip("\n"))})
+    #print(client.lista_de_produse)
+    #print(client.buget)
 
-    #creare instante CosDeCumparaturi
-    cos= CosDeCumparaturi.CosDeCumparaturi({})
+    subtotal=0
+    total=0
 
-    TOTAL=""
-    sum=0#folosite pentru generarea bonului fiscal
+    #generarea fisierelor bonuri cu numele clientului in folderul in care sunt si listele lor de cumparaturi
+    os.chdir(sys.argv[2])
+    lista = os.listdir(os.chdir(sys.argv[2]))
+    nume_client = i.lstrip("lista_cumparaturi_")
+    nume_client = nume_client.strip(".tx")
+    with open(f"bon_fiscal_{nume_client}.txt", mode="a") as f:
+        f.write(cos.elibereaza_bon())
 
-    numarul += 1
-    pozitie = 1
-
-    for x in k:# x reprezinta liniile din lista
-        if pozitie>=len(k):
-            break
-        valoare.buget = k[0]
-        a, b, c = k[pozitie].partition(',')
-        pozitie = pozitie+1
-
-        c = int(c)
-        valoare.lista_de_produse.update({a:c})#DICTIONAR
-
-    print(valoare.buget, valoare.lista_de_produse)
-    de_achitat=0
-    for i in valoare.lista_de_produse:
-        if i in produse.nume:
-            ind=produse.nume.index(i)#pentru iterare prin pozitiile listei
-
-            if produse.stoc[ind] == 0:
-                    print(f"Produsul {i} nu poate fi cumparat.")
-                    continue
-            produse.stoc[ind] = produse.stoc[ind] - valoare.lista_de_produse[i]
-            if produse.stoc[ind] < 0:
-                print("Nu mai avem cantitatea dorita in stoc.")
-                while produse.stoc[ind]<0:
-                    valoare.lista_de_produse[i]=valoare.lista_de_produse[i]-1
-                    produse.stoc[ind] = produse.stoc[ind] + 1
-                print(f"Avem ramase {valoare.lista_de_produse[i]} bucati de {i} pe care vi le putem da, in urma acestei actiuni stocul devine {produse.stoc[ind]} pentru {i}.")#am bluca infinita aici
-
-            cos.lista_de_produse.update({f"{i}": [valoare.lista_de_produse[i], produse.pret[ind]]})#cantitate, pret
-
-            if cos.lista_de_produse=={}:
-                continue#nu se face metoda de adauga_produs in cazul in care nimic nu poate fi cumparat
-            cos.adauga_produs(f"{i}",f"{valoare.lista_de_produse[i]}xbuc",f"{produse.pret[ind]*valoare.lista_de_produse[i]} lei")
-
-            de_achitat=de_achitat+valoare.lista_de_produse[i]*produse.pret[ind]
-
-            os.chdir(sys.argv[2])
-            with open(f"bon_fiscal{numarul}.txt", mode="a") as f:
-                f.write(f"{i}, {valoare.lista_de_produse[i]}xbuc, {produse.pret[ind] * valoare.lista_de_produse[i]} lei\n")
-
-            sum=sum+produse.pret[ind] * valoare.lista_de_produse[i]
-            TOTAL=f"Pret total {sum} lei"
-
+    for key, value in client.lista_de_produse.items():
+        if key in produse.nume:
+            ind=produse.nume.index(key)
         else:
-            print(f"Produsul {i} nu va exista in magazin nici dupa aprovizionare.")
+            print(f"Produsul {key} nu exista in magazin.")
+            continue
 
-    if sum==0:
-        continue#nu se genereaza fisier daca nu poate fi nimic cumparat
-    with open(f"bon_fiscal{numarul}.txt", mode="a") as f:
-        f.write(TOTAL)
+        if produse.stoc[ind] == 0:
+            print(f"Produsul {key} nu poate fi cumparat.")
+            continue
 
-    print(cos.lista_de_produse)
+        produse.stoc[ind] = produse.stoc[ind] - value#principiu:daca cantitatea dorita de client depaseste stocul
+                                                     #scadem din stoc cantitatea, stocul<0
+                                                     #adaugam 1 in stoc pana cand acesta =0, in timp ce din cantitatea dorita scadem 1
+                                                     #si obtinem cantitatea care poate fi cumparata
+        if produse.stoc[ind] < 0:
+            print("Nu mai avem cantitatea dorita in stoc.")
+            while produse.stoc[ind] < 0:
+                client.lista_de_produse[key] = client.lista_de_produse[key] - 1
+                produse.stoc[ind] = produse.stoc[ind] + 1
+                value=produse.stoc[ind]
+            #print(f"Avem ramase {client.lista_de_produse[key]} bucati de {key} pe care vi le putem da, in urma acestei actiuni stocul devine {produse.stoc[ind]} pentru {key}.")  # am bluca infinita aici
 
-    print(valoare.plateste(de_achitat))
+        cos.lista_de_produse.append([key,value])
+        cos.adauga_produs(key, client.lista_de_produse[key])
+        print(cos.lista_de_produse)
+
+        #prin client.buget-total se modifica valoarea care poate fi achitata din bugetul initial
+        #error-free pentru cazul in care bugetul este mai mic decat suma totala care ar fi putut fi achitata pentru avand in vedere cantitatea ceruta
+        if total!=0:
+            subtotal=client.plateste(client.buget-total)
+        else:
+            subtotal = client.plateste(client.buget)
+        print(f"plata subtotal {subtotal}")
+
+        with open(f"bon_fiscal_{nume_client}.txt", mode="a") as f:
+            f.write(cos.adauga_produs(key, client.lista_de_produse[key]))
+            f.write(f"....{subtotal} lei")
+            f.write("\n")
+
+        #pretul final
+        total=total+subtotal
+    with open(f"bon_fiscal_{nume_client}.txt", mode="a") as f:
+        f.write(f"Totalul este de {total} lei.")
